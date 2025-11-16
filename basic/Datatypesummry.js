4c738fd327bd3dbcce8e54fd6aa9103ad83e2bb3
Rishab Singh <rishabsingh8953@gmail.com>
2025-11-16 13:21:54 +0000

    Data Conversion

---
+++ basic/Datatypesummry.js
@@ -0,0 +1,16 @@
+const id =Symbol("123");    
+const id2 =Symbol("123");
+
+console.log (id === id2); // false
+
+const bigNumber = 1234567890123456789012345678901234567890n;
+console.log (bigNumber);
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:"Ravi",
+    age:24,
+};
+
+console.log (heros);
+console.log (myObj);
